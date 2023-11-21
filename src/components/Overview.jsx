e249ee665911d223db345e8b3f290d8446579681
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-11-21 10:56:00 +0100

    overview part 2

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,7 +1,7 @@
 function Overview() {
   return (
-    <section className="mx-auto flex max-w-[90rem] items-center justify-center py-10">
-      <section className="relative">
+    <section className="mx-auto flex max-w-[70rem] items-center justify-center gap-[7rem] py-16">
+      <section className="relative w-1/2">
         <img className="w-full" src="/the big idea.png" alt="Idea" />
         <img className="absolute -left-20 top-40" src="/star.png" alt="star" />
         <img
@@ -14,7 +14,22 @@ function Overview() {
         </p>
       </section>
 
-      <section></section>
+      <section className="flex w-1/2 flex-col gap-6">
+        <span className="relative">
+          <h2 className="font-clash text-[2rem] font-bold text-white">
+            Introduction to getlinked <br />
+            <span className="text-secondary">tech Hackathon 1.0</span>
+          </h2>
+          <img
+            className="absolute -right-6 top-1/3"
+            src="/star.png"
+            alt="star"
+          />
+        </span>
+        <p className="font-mont text-sm font-normal leading-6 text-white">
+          {`Our tech hackathon is a melting pot of visionaries, and its purpose is as clear as day: to shape the future. Whether you're a coding genius, a design maverick, or a concept wizard, you'll have the chance to transform your ideas into reality. Solving real-world problems, pushing the boundaries of technology, and creating solutions that can change the world, that's what we're all about!`}
+        </p>
+      </section>
     </section>
   );
 }

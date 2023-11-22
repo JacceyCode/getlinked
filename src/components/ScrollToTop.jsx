61f9cc15d1bf2649f3e43f8ac3aab29cf9f06803
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-11-22 12:14:21 +0100

    edited scrollToTop

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -20,7 +20,7 @@ function ScrollToTop() {
     <>
       {showTopBtn && (
         <FaArrowUpLong
-          className="bg-primary hover:bg-secondary fixed bottom-2 right-2 z-10 h-10 w-10 animate-bounce cursor-pointer rounded-lg p-3 text-white"
+          className="bg-primary hover:bg-secondary fixed bottom-2 right-2 z-10 h-10 w-10 cursor-pointer rounded-lg p-3 text-white"
           onClick={goToTop}
         />
       )}
